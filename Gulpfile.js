65049351450505f9b72e1e2d9a601e0a8fd7aa12
tyxchen <tyxchen@outlook.com>
2015-12-13 21:36:20 -0500

    Add Autoprefixer to Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,12 @@
 
 'use strict';
 
+// If NodeJS version is 0.10.x, use Promise polyfill to support Autoprefixer
+if (/0.10/.test(process.version)) {
+  console.log("NodeJS 0.10.x detected; using Promise polyfill");
+  require('es6-promise').polyfill();
+}
+
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     auto = require('gulp-autoprefixer'),
@@ -21,6 +27,7 @@ var gulp = require('gulp'),
 gulp.task('build:sass', function () {
   return sass('src/main.scss', {sourcemap: true, style: 'expanded'})
     .on('error', function (e) { console.error(e.message); })
+    .pipe(auto())
     .pipe(maps.write({sourceRoot: './'}))
     .pipe(name({basename: 'fresh'}))
     .pipe(gulp.dest('build/'));
@@ -36,4 +43,3 @@ gulp.task('default', ['build:sass', 'watch']);
 gulp.task('watch', function () {
   gulp.watch(['src/**/*.scss', 'src/main.scss'], ['build:sass']);
 });
-
